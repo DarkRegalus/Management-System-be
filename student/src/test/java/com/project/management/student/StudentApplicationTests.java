a967c445dc6b4ab6996beccae0027219ebd80980
Arkojeet <53788782+Arkojeet@users.noreply.github.com>
2022-10-30 17:10:40 +0530

    first commit

---
+++ student/src/test/java/com/project/management/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.management.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
